9e5009385830eda21cff739a2436e1e33226e731
sean-wei <309528163@qq.com>
2019-11-13 14:31:34 +0800

    add driver&owner

--- application/database.php
+++ application/database.php
@@ -18,7 +18,7 @@ return [
     // 服务器地址
     'hostname'        => Env::get('database.hostname', '127.0.0.1'),
     // 数据库名
-    'database'        => Env::get('database.database', 'fastadmin_new'),
+    'database'        => Env::get('database.database', 'fastadmin_xk'),
     // 用户名
     'username'        => Env::get('database.username', 'root'),
     // 密码
